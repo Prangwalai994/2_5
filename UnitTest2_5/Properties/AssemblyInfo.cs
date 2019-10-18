850580cbb489eb9a8301b282e13ba740847bff8b
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-18 19:59:12 +0700

    Add project files.

---
+++ UnitTest2_5/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest2_5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest2_5")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da328287-4754-4476-9ecf-cd54413347ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest2_5/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using IntroducingRider2_5.Fundamentals;
+using NUnit.Framework;
+
+namespace UnitTest2_5
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrage
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+        [Test]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_Return()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_RetureFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+
+        }
+
+
+        [Test]
+        public void CanBeCancelledBy_SameUseCancelling_ReturnTrue()
+        {
+
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnFalse()
+        {
+
+        }
+
+    }
+
+    class TestFixtureAttribute : Attribute
+    {
+    }
+}
